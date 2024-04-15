8b6dd769517641cbae5678658a5faf1ef6ff49a5
SRobb64 <PAP0205443@lpssonline.com>
2024-04-15 13:00:20 +0000

    saving data shapes

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,13 +10,23 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var KEY = {
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+  }
   // Game Item Objects
-
+  var walker = {
+    x: 0
+    y: 0
+    speedX: 0
+    speedY: 0
+  }
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,8 +44,23 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    if (event.which === KEY.UP) {
+      console.log(event.which + " pressed")
+      walker.speedY -= 5 
+    }
+    if (event.which === KEY.DOWN) {
+      console.log(event.which + " pressed")
+      walker.speedY += 5 
+    }
+    if (event.which === KEY.LEFT) {
+      console.log(event.which + " pressed")
+      walker.speedX -= 5 
+    }
+    if (event.which === KEY.RIGHT) {
+      console.log(event.which + " pressed")
+      walker.speedX += 5 
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -51,4 +76,15 @@ function runProgram(){
     $(document).off();
   }
   
+
+  function repositionGameItem(){
+    walker.x += speedX
+    walker.y += speedY
+  }
+
+  function redrawGameItem(){
+    $("#board").css("left", walker.x)
+    $("#board").css("top", walker.y)
+
+  }
 }
